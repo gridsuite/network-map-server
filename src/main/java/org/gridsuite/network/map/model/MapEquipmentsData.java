2f660029fcbb991dd80c23593c92a9552015197f
Hugo Marcellin <hugo.marcelin@rte-france.com>
2022-11-14 14:32:23 +0100

    Remove imports

--- src/main/java/org/gridsuite/network/map/model/MapEquipmentsData.java
+++ src/main/java/org/gridsuite/network/map/model/MapEquipmentsData.java
@@ -3,8 +3,6 @@ package org.gridsuite.network.map.model;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import org.gridsuite.network.map.model.LineMapData;
-import org.gridsuite.network.map.model.SubstationMapData;
 
 import java.util.List;
 
