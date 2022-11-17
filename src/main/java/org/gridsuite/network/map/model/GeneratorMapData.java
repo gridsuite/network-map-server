3df62ad80c2731e153b6644c9ba623ca89db8c39
Etienne Homer <etiennehomer@gmail.com>
2022-11-17 17:18:16 +0100

    Add comment for lombok/jackson problem with qPercent
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/network/map/model/GeneratorMapData.java
+++ src/main/java/org/gridsuite/network/map/model/GeneratorMapData.java
@@ -98,6 +98,8 @@ public class GeneratorMapData {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String connectionName;
 
+    // As this attribute has only one lower case letter at its start (xXXXX), the getters is parsed as getQPercent and the field for Jackson is parsed as qpercent
+    // while we expect qPercent. JsonProperty let fix the json field to qPercent
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("qPercent")
     @Getter(AccessLevel.NONE)
