4a1810430783e9fe64118819de43b5055bb6f2ac
etiennehomer <etiennehomer@gmail.com>
2023-02-08 10:54:33 +0100

    Add branch status to basic lines data (#91)
    Signed-off-by: HOMER Etienne <etiennehomer@gmail.com>

--- src/main/java/org/gridsuite/network/map/NetworkMapService.java
+++ src/main/java/org/gridsuite/network/map/NetworkMapService.java
@@ -163,6 +163,10 @@ class NetworkMapService {
         if (limits2 != null && !Double.isNaN(limits2.getPermanentLimit())) {
             builder.permanentLimit2(limits2.getPermanentLimit());
         }
+        BranchStatus<Line> branchStatus = line.getExtension(BranchStatus.class);
+        if (branchStatus != null) {
+            builder.branchStatus(branchStatus.getStatus().name());
+        }
         return builder.build();
     }
 
