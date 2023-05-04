aa16bcbfecb876493e76627d29e345b9b6d2d7cd
AAJELLAL <ali.ajellalod@gmail.com>
2023-05-04 16:09:49 +0200

    Add ThreeWindingsTransformerFormInfos dto.
    Signed-off-by: AAJELLAL <ali.ajellalod@gmail.com>

--- src/main/java/org/gridsuite/network/map/dto/ElementInfos.java
+++ src/main/java/org/gridsuite/network/map/dto/ElementInfos.java
@@ -11,10 +11,13 @@ import com.powsybl.iidm.network.*;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 import org.gridsuite.network.map.model.CurrentLimitsData;
+import org.gridsuite.network.map.model.TapChangerData;
+import org.gridsuite.network.map.model.TapChangerStepData;
 import org.gridsuite.network.map.model.TemporaryLimitData;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -89,5 +92,85 @@ public class ElementInfos {
         BusbarSectionFinderTraverser connectedBusbarSectionFinder = new BusbarSectionFinderTraverser(terminal.isConnected());
         terminal.traverse(connectedBusbarSectionFinder);
         return connectedBusbarSectionFinder.getFirstTraversedBbsId();
+    }
+
+    protected static List<TapChangerStepData> toMapDataPhaseStep(Map<Integer, PhaseTapChangerStep> tapChangerStep) {
+        if (tapChangerStep == null) {
+            return List.of();
+        }
+        return tapChangerStep.entrySet().stream().map(p -> {
+            Integer index = p.getKey();
+            PhaseTapChangerStep v = p.getValue();
+
+            return TapChangerStepData.builder().index(index)
+                    .g(v.getG())
+                    .b(v.getB())
+                    .r(v.getR())
+                    .x(v.getX())
+                    .rho(v.getRho())
+                    .alpha(v.getAlpha())
+                    .build();
+        }).collect(Collectors.toList());
+    }
+
+    protected static TapChangerData toMapData(PhaseTapChanger tapChanger) {
+        if (tapChanger == null) {
+            return null;
+        }
+
+        TapChangerData.TapChangerDataBuilder builder = TapChangerData.builder()
+                .lowTapPosition(tapChanger.getLowTapPosition())
+                .highTapPosition(tapChanger.getHighTapPosition())
+                .tapPosition(tapChanger.getTapPosition())
+                .regulating(tapChanger.isRegulating())
+                .regulationMode(tapChanger.getRegulationMode())
+                .regulationValue(tapChanger.getRegulationValue())
+                .targetDeadBand(tapChanger.getTargetDeadband())
+                .regulatingTerminalConnectableId(tapChanger.getRegulationTerminal() != null ? tapChanger.getRegulationTerminal().getConnectable().getId() : null)
+                .regulatingTerminalConnectableType(tapChanger.getRegulationTerminal() != null ? tapChanger.getRegulationTerminal().getConnectable().getType().name() : null)
+                .regulatingTerminalVlId(tapChanger.getRegulationTerminal() != null ? tapChanger.getRegulationTerminal().getVoltageLevel().getId() : null)
+                .steps(toMapDataPhaseStep(tapChanger.getAllSteps()));
+
+        builder.targetDeadBand(nullIfNan(tapChanger.getTargetDeadband()));
+        return builder.build();
+    }
+
+    protected static TapChangerData toMapData(RatioTapChanger tapChanger) {
+        if (tapChanger == null) {
+            return null;
+        }
+
+        TapChangerData.TapChangerDataBuilder builder = TapChangerData.builder()
+                .lowTapPosition(tapChanger.getLowTapPosition())
+                .highTapPosition(tapChanger.getHighTapPosition())
+                .tapPosition(tapChanger.getTapPosition())
+                .regulating(tapChanger.isRegulating())
+                .loadTapChangingCapabilities(tapChanger.hasLoadTapChangingCapabilities())
+                .regulatingTerminalConnectableId(tapChanger.getRegulationTerminal() != null ? tapChanger.getRegulationTerminal().getConnectable().getId() : null)
+                .regulatingTerminalConnectableType(tapChanger.getRegulationTerminal() != null ? tapChanger.getRegulationTerminal().getConnectable().getType().name() : null)
+                .regulatingTerminalVlId(tapChanger.getRegulationTerminal() != null ? tapChanger.getRegulationTerminal().getVoltageLevel().getId() : null)
+                .steps(toMapDataRatioStep(tapChanger.getAllSteps()));
+
+        builder.targetV(nullIfNan(tapChanger.getTargetV()));
+        builder.targetDeadBand(nullIfNan(tapChanger.getTargetDeadband()));
+        return builder.build();
+    }
+
+    protected static List<TapChangerStepData> toMapDataRatioStep(Map<Integer, RatioTapChangerStep> tapChangerStep) {
+        if (tapChangerStep == null) {
+            return List.of();
+        }
+        return tapChangerStep.entrySet().stream().map(p -> {
+            Integer index = p.getKey();
+            RatioTapChangerStep v = p.getValue();
+
+            return TapChangerStepData.builder().index(index)
+                    .g(v.getG())
+                    .b(v.getB())
+                    .r(v.getR())
+                    .x(v.getX())
+                    .rho(v.getRho())
+                    .build();
+        }).collect(Collectors.toList());
     }
 }
