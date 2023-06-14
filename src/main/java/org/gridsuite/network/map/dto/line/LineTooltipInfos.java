9e8128967444e91835ae6198e93c9227e299a5a9
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2023-06-14 16:44:43 +0200

    feat: new infoType tooltip
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

---
+++ src/main/java/org/gridsuite/network/map/dto/line/LineTooltipInfos.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.network.map.dto.line;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.powsybl.iidm.network.Identifiable;
+import com.powsybl.iidm.network.Line;
+import com.powsybl.iidm.network.Terminal;
+import com.powsybl.iidm.network.extensions.BranchStatus;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.gridsuite.network.map.model.CurrentLimitsData;
+
+import static org.gridsuite.network.map.dto.utils.ElementUtils.nullIfNan;
+import static org.gridsuite.network.map.dto.utils.ElementUtils.toMapDataCurrentLimits;
+
+/**
+ * @author Le Saulnier Kevin <kevin.lesaulnier at rte-france.com>
+ */
+@SuperBuilder
+@Getter
+public class LineTooltipInfos extends AbstractLineInfos {
+
+    private String voltageLevelId1;
+
+    private String voltageLevelId2;
+
+    private Boolean terminal1Connected;
+
+    private Boolean terminal2Connected;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double p1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double p2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double i1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double i2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private CurrentLimitsData currentLimits1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private CurrentLimitsData currentLimits2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String branchStatus;
+
+    public static LineTooltipInfos toData(Identifiable<?> identifiable) {
+        Line line = (Line) identifiable;
+        Terminal terminal1 = line.getTerminal1();
+        Terminal terminal2 = line.getTerminal2();
+
+        LineTooltipInfos.LineTooltipInfosBuilder builder = LineTooltipInfos.builder()
+                .id(line.getId())
+                .name(line.getOptionalName().orElse(null))
+                .terminal1Connected(terminal1.isConnected())
+                .terminal2Connected(terminal2.isConnected())
+                .voltageLevelId1(terminal1.getVoltageLevel().getId())
+                .voltageLevelId2(terminal2.getVoltageLevel().getId())
+                .i1(nullIfNan(terminal1.getI()))
+                .i2(nullIfNan(terminal2.getI()))
+                .p1(nullIfNan(terminal1.getP()))
+                .p2(nullIfNan(terminal2.getP()));
+
+        line.getCurrentLimits1().ifPresent(limits1 -> builder.currentLimits1(toMapDataCurrentLimits(limits1)));
+        line.getCurrentLimits2().ifPresent(limits2 -> builder.currentLimits2(toMapDataCurrentLimits(limits2)));
+
+        BranchStatus<Line> branchStatus = line.getExtension(BranchStatus.class);
+        if (branchStatus != null) {
+            builder.branchStatus(branchStatus.getStatus().name());
+        }
+
+        return builder.build();
+    }
+}
--- src/main/java/org/gridsuite/network/map/dto/twowindingstransformer/TwoWindingsTransformerTooltipInfos.java
+++ src/main/java/org/gridsuite/network/map/dto/twowindingstransformer/TwoWindingsTransformerTooltipInfos.java
@@ -11,7 +11,11 @@ import static org.gridsuite.network.map.dto.utils.ElementUtils.*;
 
 @SuperBuilder
 @Getter
-public class TwoWindingsTransformerMapInfos extends AbstractTwoWindingsTransformerInfos {
+public class TwoWindingsTransformerTooltipInfos extends AbstractTwoWindingsTransformerInfos {
+
+    private String voltageLevelId1;
+
+    private String voltageLevelId2;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Double p1;
@@ -34,14 +38,16 @@ public class TwoWindingsTransformerMapInfos extends AbstractTwoWindingsTransform
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String branchStatus;
 
-    public static TwoWindingsTransformerMapInfos toData(Identifiable<?> identifiable) {
+    public static TwoWindingsTransformerTooltipInfos toData(Identifiable<?> identifiable) {
         TwoWindingsTransformer twoWindingsTransformer = (TwoWindingsTransformer) identifiable;
         Terminal terminal1 = twoWindingsTransformer.getTerminal1();
         Terminal terminal2 = twoWindingsTransformer.getTerminal2();
 
-        TwoWindingsTransformerMapInfos.TwoWindingsTransformerMapInfosBuilder builder = TwoWindingsTransformerMapInfos.builder()
+        TwoWindingsTransformerTooltipInfos.TwoWindingsTransformerTooltipInfosBuilder builder = TwoWindingsTransformerTooltipInfos.builder()
             .id(twoWindingsTransformer.getId())
             .name(twoWindingsTransformer.getOptionalName().orElse(null))
+            .voltageLevelId1(terminal1.getVoltageLevel().getId())
+            .voltageLevelId2(terminal2.getVoltageLevel().getId())
             .i1(nullIfNan(terminal1.getI()))
             .i2(nullIfNan(terminal2.getI()))
             .p1(nullIfNan(terminal1.getP()))
