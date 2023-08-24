5f96af27fbfe9fcb49262ef549796eca3945786c
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2023-08-24 16:48:56 +0200

    Get low voltage limit, high voltage limit, icc min and icc max values for voltage level spreadsheet (#133)
    * get low voltage limit, high voltage limit, icc min and icc max values to show them in voltage levels spreadsheet
    
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/network/map/dto/voltagelevel/VoltageLevelTabInfos.java
+++ src/main/java/org/gridsuite/network/map/dto/voltagelevel/VoltageLevelTabInfos.java
@@ -9,8 +9,11 @@ package org.gridsuite.network.map.dto.voltagelevel;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.powsybl.iidm.network.Identifiable;
 import com.powsybl.iidm.network.VoltageLevel;
+import com.powsybl.iidm.network.extensions.IdentifiableShortCircuit;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
+
+import static org.gridsuite.network.map.dto.utils.ElementUtils.nullIfNan;
 
 /**
  * @author Slimane Amar <slimane.amar at rte-france.com>
@@ -24,13 +27,40 @@ public class VoltageLevelTabInfos extends AbstractVoltageLevelInfos {
 
     private double nominalVoltage;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double lowVoltageLimit;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double highVoltageLimit;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double ipMin;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double ipMax;
+
     public static VoltageLevelTabInfos toData(Identifiable<?> identifiable) {
         VoltageLevel voltageLevel = (VoltageLevel) identifiable;
-        return VoltageLevelTabInfos.builder()
-                .id(voltageLevel.getId())
-                .name(voltageLevel.getOptionalName().orElse(null))
-                .substationId(voltageLevel.getSubstation().orElseThrow().getId())
-                .nominalVoltage(voltageLevel.getNominalV())
-                .build();
+        VoltageLevelTabInfos.VoltageLevelTabInfosBuilder builder = VoltageLevelTabInfos.builder()
+            .id(voltageLevel.getId())
+            .name(voltageLevel.getOptionalName().orElse(null))
+            .substationId(voltageLevel.getSubstation().orElseThrow().getId())
+            .nominalVoltage(voltageLevel.getNominalV())
+            .lowVoltageLimit(nullIfNan(voltageLevel.getLowVoltageLimit()))
+            .highVoltageLimit(nullIfNan(voltageLevel.getHighVoltageLimit()));
+
+        IdentifiableShortCircuit<VoltageLevel> identifiableShortCircuit = voltageLevel.getExtension(IdentifiableShortCircuit.class);
+        if (identifiableShortCircuit != null) {
+            Double ipMin = nullIfNan(identifiableShortCircuit.getIpMin());
+            if (ipMin != null) {
+                builder.ipMin(ipMin / 1000.);  // to get value in kA : value is in A in iidm
+            }
+            Double ipMax = nullIfNan(identifiableShortCircuit.getIpMax());
+            if (ipMax != null) {
+                builder.ipMax(ipMax / 1000.);  // to get value in kA : value is in A in iidm
+            }
+        }
+
+        return builder.build();
     }
 }
