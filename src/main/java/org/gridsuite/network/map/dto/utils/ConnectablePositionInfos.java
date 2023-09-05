ac107c8d6844246fe1ef208195fb4c76372528b2
Maissa SOUISSI <maissa.souissi@rte-france.com>
2023-09-05 17:25:42 +0200

    fix
    Signed-off-by: Maissa SOUISSI <maissa.souissi@rte-france.com>

--- src/main/java/org/gridsuite/network/map/dto/utils/ConnectablePositionInfos.java
+++ src/main/java/org/gridsuite/network/map/dto/utils/ConnectablePositionInfos.java
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.powsybl.iidm.network.extensions.ConnectablePosition;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 public class ConnectablePositionInfos {
     private ConnectablePosition.Direction connectionDirection;
