99bb0535b185b3c526f96d123d7e3fa775f1e7c8
Joris Mancini <joris.mancini_externe@rte-france.com>
2024-01-18 10:58:57 +0100

    chore: add copyright header

--- src/main/java/org/gridsuite/network/map/dto/ElementFormInfosWithProperties.java
+++ src/main/java/org/gridsuite/network/map/dto/ElementFormInfosWithProperties.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package org.gridsuite.network.map.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -6,6 +12,9 @@ import lombok.experimental.SuperBuilder;
 
 import java.util.Map;
 
+/**
+ * @author Joris Mancini <joris.mancini_externe at rte-france.com>
+ */
 @SuperBuilder
 @Getter
 public class ElementFormInfosWithProperties extends ElementInfos {
