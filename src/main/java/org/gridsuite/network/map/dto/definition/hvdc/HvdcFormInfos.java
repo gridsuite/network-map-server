8a4dc2d3781091ad9fe03bcf2fd83d0d6ebc030e
TOURI ANIS <anis-1.touri@rte-france.com>
2024-03-06 18:10:15 +0100

    change dcResistance and dcNominalVoltage fields
    Signed-off-by: TOURI ANIS <anis-1.touri@rte-france.com>

--- src/main/java/org/gridsuite/network/map/dto/definition/hvdc/HvdcFormInfos.java
+++ src/main/java/org/gridsuite/network/map/dto/definition/hvdc/HvdcFormInfos.java
@@ -35,10 +35,10 @@ public class HvdcFormInfos extends ElementInfos {
     private VscConverterStationFormInfos converterStation2;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private Double dcResistance;
+    private Double r;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private Double dcNominalVoltage;
+    private Double nominalV;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Double activePowerSetpoint;
