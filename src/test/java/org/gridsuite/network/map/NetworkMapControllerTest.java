4ed363bc3a9e83c8a5ea548a7a7f618a82f4363c
Franck LECUYER <franck.lecuyer@rte-france.com>
2024-04-05 13:42:25 +0200

    implements TAB view for tie lines
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/org/gridsuite/network/map/NetworkMapControllerTest.java
+++ src/test/java/org/gridsuite/network/map/NetworkMapControllerTest.java
@@ -1967,4 +1967,10 @@ public class NetworkMapControllerTest {
         succeedingTestForElementsInfos(NETWORK_2_UUID, null, ElementType.BUS, InfoType.TAB, null, resourceToString("/buses-tab-data.json"));
         succeedingTestForElementsInfos(NETWORK_2_UUID, null, ElementType.BUS, InfoType.TAB, List.of("n9828181c-7977-4592-ba19-008976e4254e_substation1"), resourceToString("/buses-tab-data.json"));
     }
+
+    @Test
+    public void shouldReturnTieLinesTabData() throws Exception {
+        succeedingTestForElementsInfos(NETWORK_UUID, null, ElementType.TIE_LINE, InfoType.TAB, null, resourceToString("/tie-lines-tab-data.json"));
+        succeedingTestForElementsInfos(NETWORK_UUID, VARIANT_ID, ElementType.TIE_LINE, InfoType.TAB, null, resourceToString("/tie-lines-tab-data.json"));
+    }
 }
