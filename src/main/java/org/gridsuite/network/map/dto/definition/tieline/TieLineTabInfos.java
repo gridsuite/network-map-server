f4b592cff99dfc4e78f6a8910620f512ed6b3ac3
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2024-04-09 12:57:32 +0200

    implements TAB view for tie lines (#179)
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

---
+++ src/main/java/org/gridsuite/network/map/dto/definition/tieline/TieLineTabInfos.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2024, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.network.map.dto.definition.tieline;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.powsybl.iidm.network.Country;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.gridsuite.network.map.dto.ElementTabInfoWithProperties;
+
+/**
+ * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
+ */
+@SuperBuilder
+@Getter
+public class TieLineTabInfos extends ElementTabInfoWithProperties {
+
+    private String voltageLevelId1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String voltageLevelName1;
+
+    private Double nominalVoltage1;
+
+    private String voltageLevelId2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String voltageLevelName2;
+
+    private Double nominalVoltage2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Country country1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Country country2;
+
+    private Boolean terminal1Connected;
+
+    private Boolean terminal2Connected;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double p1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double q1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double p2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double q2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double r;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double x;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double g1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double b1;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double g2;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double b2;
+}
