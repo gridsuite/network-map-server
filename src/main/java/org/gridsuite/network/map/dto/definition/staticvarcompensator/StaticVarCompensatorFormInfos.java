051b0cdb8be058c648e9c15be8b0a3554bd1c497
Rehili Ghazwa <ghazwarhili@gmail.com>
2024-08-21 09:13:30 +0200

    add form type for static var compensator

---
+++ src/main/java/org/gridsuite/network/map/dto/definition/staticvarcompensator/StaticVarCompensatorFormInfos.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.gridsuite.network.map.dto.definition.staticvarcompensator;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.powsybl.iidm.network.Country;
+import com.powsybl.iidm.network.StaticVarCompensator;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import org.gridsuite.network.map.dto.ElementInfosWithProperties;
+import org.gridsuite.network.map.dto.definition.extension.ConnectablePositionInfos;
+
+/**
+ * @author REHILI Ghazwa <ghazwarhili@gmail.com>
+ */
+@SuperBuilder
+@Getter
+public class StaticVarCompensatorFormInfos extends ElementInfosWithProperties {
+    private String voltageLevelId;
+
+    private Double nominalV; // nominal voltage of the voltage level of the regulating terminal
+
+    private Boolean terminalConnected;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double bmin;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double bmax;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double voltageSetpoint;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Double reactivePowerSetpoint;
+
+    private StaticVarCompensator.RegulationMode regulationMode;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String busOrBusbarSectionId;
+
+    private ConnectablePositionInfos connectablePosition;
+
+}
--- src/main/java/org/gridsuite/network/map/dto/mapper/StaticVarCompensatorInfosMapper.java
+++ src/main/java/org/gridsuite/network/map/dto/mapper/StaticVarCompensatorInfosMapper.java
@@ -6,15 +6,15 @@
  */
 package org.gridsuite.network.map.dto.mapper;
 
-import com.powsybl.iidm.network.Identifiable;
-import com.powsybl.iidm.network.StaticVarCompensator;
-import com.powsybl.iidm.network.Terminal;
+import com.powsybl.iidm.network.*;
 import org.gridsuite.network.map.dto.ElementInfos;
 import org.gridsuite.network.map.dto.InfoTypeParameters;
+import org.gridsuite.network.map.dto.definition.shuntcompensator.ShuntCompensatorFormInfos;
+import org.gridsuite.network.map.dto.definition.staticvarcompensator.StaticVarCompensatorFormInfos;
 import org.gridsuite.network.map.dto.definition.staticvarcompensator.StaticVarCompensatorTabInfos;
 
-import static org.gridsuite.network.map.dto.utils.ElementUtils.getProperties;
-import static org.gridsuite.network.map.dto.utils.ElementUtils.mapCountry;
+import static org.gridsuite.network.map.dto.utils.ElementUtils.*;
+import static org.gridsuite.network.map.dto.utils.ElementUtils.toMapConnectablePosition;
 
 /**
  * @author AJELLAL Ali <ali.ajellal@rte-france.com>
@@ -27,11 +27,33 @@ public final class StaticVarCompensatorInfosMapper {
         switch (infoTypeParameters.getInfoType()) {
             case TAB:
                 return toTabInfos(identifiable);
+            case FORM:
+                return toFormInfos(identifiable);
             case LIST:
                 return ElementInfosMapper.toInfosWithType(identifiable);
             default:
                 throw new UnsupportedOperationException("TODO");
         }
+    }
+
+    private static StaticVarCompensatorFormInfos toFormInfos(Identifiable<?> identifiable) {
+        StaticVarCompensator staticVarCompensator = (StaticVarCompensator) identifiable;
+        Terminal terminal = staticVarCompensator.getTerminal();
+        StaticVarCompensatorFormInfos.StaticVarCompensatorFormInfosBuilder<?, ?> builder = StaticVarCompensatorFormInfos.builder()
+                .name(staticVarCompensator.getOptionalName().orElse(null))
+                .id(staticVarCompensator.getId())
+                .terminalConnected(terminal.isConnected())
+                .voltageLevelId(terminal.getVoltageLevel().getId())
+                .regulationMode(staticVarCompensator.getRegulationMode())
+                .bmax(staticVarCompensator.getBmax())
+                .bmin(staticVarCompensator.getBmin())
+                .voltageSetpoint(staticVarCompensator.getVoltageSetpoint())
+                .reactivePowerSetpoint(staticVarCompensator.getReactivePowerSetpoint())
+                .busOrBusbarSectionId(getBusOrBusbarSection(terminal))
+                .properties(getProperties(staticVarCompensator))
+                .connectablePosition(toMapConnectablePosition(staticVarCompensator, 0));
+
+        return builder.build();
     }
 
     private static StaticVarCompensatorTabInfos toTabInfos(Identifiable<?> identifiable) {
