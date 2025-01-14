455e505f2b7103e15550c04da8b543157e5b0ed4
Franck LECUYER <franck.lecuyer@rte-france.com>
2025-01-14 09:18:01 +0100

    Fix getting measurements for a branch or a 3wt on side other than 1, when there are measurements on multiple sides.
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/main/java/org/gridsuite/network/map/dto/utils/ElementUtils.java
+++ src/main/java/org/gridsuite/network/map/dto/utils/ElementUtils.java
@@ -395,7 +395,7 @@ public final class ElementUtils {
         if (measurements == null) {
             return Optional.empty();
         } else {
-            return measurements.getMeasurements(type).stream().findFirst().filter(m -> ((Measurement) m).getSide() == null || ((Measurement) m).getSide().getNum() - 1 == index)
+            return measurements.getMeasurements(type).stream().filter(m -> ((Measurement) m).getSide() == null || ((Measurement) m).getSide().getNum() - 1 == index).findFirst()
                 .map(m -> MeasurementsInfos.builder().value(((Measurement) m).getValue()).validity(((Measurement) m).isValid()).build());
         }
     }
