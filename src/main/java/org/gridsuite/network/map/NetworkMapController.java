032f07ae24d6bdfbc0dac16ac2ebb0dfbccb3e88
Rehili Ghazwa <ghazwarhili@gmail.com>
2025-02-03 18:10:21 +0100

    enhance the param description

--- src/main/java/org/gridsuite/network/map/NetworkMapController.java
+++ src/main/java/org/gridsuite/network/map/NetworkMapController.java
@@ -53,7 +53,7 @@ public class NetworkMapController {
                                        @Parameter(description = "Variant Id") @RequestParam(name = "variantId", required = false) String variantId,
                                        @Parameter(description = "Nominal Voltages") @RequestParam(name = "nominalVoltages", required = false) List<Double> nominalVoltages,
                                        @Parameter(description = "Element type") @RequestParam(name = "elementType") ElementType elementType,
-                                       @Parameter(description = "Element type") @RequestParam(name = "equipmentSubType", required = false) String equipmentSubType,
+                                       @Parameter(description = "Equipment SubType") @RequestParam(name = "equipmentSubType", required = false) String equipmentSubType,
                                        @RequestBody(required = false) Optional<List<String>> substationsIds) {
         return networkMapService.getElementsIds(networkUuid, variantId, substationsIds.orElseGet(List::of), elementType, equipmentSubType, nominalVoltages);
     }
