0e41299b947099ab3c388bf6fe15fc82ab3cb440
Thang PHAM <phamthang37@gmail.com>
2025-04-03 20:36:53 +0200

    revert NullAndNaNFilter in java normal
    Signed-off-by: Thang PHAM <phamthang37@gmail.com>

--- src/main/java/org/gridsuite/network/map/config/nan/NullAndNaNFilter.java
+++ src/main/java/org/gridsuite/network/map/config/nan/NullAndNaNFilter.java
@@ -4,11 +4,13 @@ public class NullAndNaNFilter {
     @SuppressWarnings("checkstyle:EqualsHashCode")
     @Override
     public boolean equals(Object value) {
-        return switch (value) {
-            case null -> true; // Ignore null
-            case Double doubleValue when doubleValue.isNaN() -> true; // Ignore NaN
-            case Float floatValue when floatValue.isNaN() -> true; // Ignore NaN
-            default -> false;
-        };
+        if (value == null) {
+            return true; // Ignore null
+        } else if (value instanceof Double doubleValue && doubleValue.isNaN()) {
+            return true; // Ignore NaN
+        } else if (value instanceof Float floatValue && floatValue.isNaN()) {
+            return true; // Ignore NaN
+        }
+        return false;
     }
 }
