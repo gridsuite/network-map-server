232179ce893bd985e1cb2d1df920971182180cb4
Hugo Marcellin <hugo.marcelin@rte-france.com>
2025-08-28 14:50:07 +0200

    Use mockbean instead of spybean

--- src/test/java/org/gridsuite/network/map/SchemaControllerTest.java
+++ src/test/java/org/gridsuite/network/map/SchemaControllerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
@@ -31,7 +32,7 @@ class SchemaControllerTest implements WithAssertions {
     @Autowired
     private MockMvc mockMvc;
 
-    @SpyBean //need to init it for the controller
+    @MockBean //need to init it for the controller
     private SchemaService schemaService;
 
     final ClassLoader cl = this.getClass().getClassLoader();
