29f95d1e5b9fb02632c77891c569715b11a91a97
Etienne Homer <etiennehomer@gmail.com>
2025-10-03 11:14:58 +0200

    import
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/test/java/org/gridsuite/network/map/mapper/BusbarSectionFinderTraverserTest.java
+++ src/test/java/org/gridsuite/network/map/mapper/BusbarSectionFinderTraverserTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Properties;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
